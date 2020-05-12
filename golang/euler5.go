178b02f0f533a0163c999840740b58e9cf2eb219
gwg <gwg@gwgs-MacBook-Pro.local>
2020-05-11 20:12:58 -0400

    factor out the logic for euler5

--- golang/euler5.go
+++ golang/euler5.go
@@ -1,3 +1,5 @@
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 package main
 
 import (
@@ -9,8 +11,7 @@ import (
 	"./primefactorize"
 )
 
-func main() {
-	max_factor := 20
+func euler5(max_factor int) int {
 	products := make(map[int]int)
 	for i := 2; i <= max_factor; i++ {
 		factors := primefactorize.Primefactorize(i)
@@ -22,5 +23,10 @@ func main() {
 	for factor, count := range products {
 		result = result * numberutil.Pow_int_to_int(factor, count)
 	}
-	fmt.Println(result)
+	return result
+}
+
+func main() {
+	fmt.Println(euler5(10))
+	fmt.Println(euler5(20))
 }
